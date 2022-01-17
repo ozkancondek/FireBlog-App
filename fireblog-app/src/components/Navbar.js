6a44d0a5d96e8dffa88d5ce9aad637af99db5a47
ozkancondek <ozkncndek@gmail.com>
2022-01-17 20:22:27 +0100

    authentication

--- fireblog-app/src/components/Navbar.js
+++ fireblog-app/src/components/Navbar.js
@@ -6,19 +6,44 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import Switch from "@mui/material/Switch";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormGroup from "@mui/material/FormGroup";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
+import { makeStyles } from "@mui/styles";
+import cwLogo from "../assets/cw.jpeg";
 
-export default function MenuAppBar() {
-  const [auth, setAuth] = React.useState(true);
+//https://mui.com/styles/basics/
+const useStyles = makeStyles(() => ({
+  menuButton: {},
+  title: {
+    fontFamily: "Girassol",
+
+    "& span": {
+      fontSize: 30,
+      color: "wheat",
+    },
+  },
+  toolbar: {
+    backgroundColor: "#046582",
+  },
+  logo: {
+    width: 40,
+  },
+  linkStyle: {
+    textDecoration: "none",
+    color: "black",
+  },
+
+  login: {
+    padding: 10,
+    fontSize: 20,
+    color: "white",
+    textDecoration: "none",
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleChange = (event) => {
-    setAuth(event.target.checked);
-  };
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
@@ -30,20 +55,8 @@ export default function MenuAppBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <FormGroup>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={auth}
-              onChange={handleChange}
-              aria-label="login switch"
-            />
-          }
-          label={auth ? "Logout" : "Login"}
-        />
-      </FormGroup>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar className={classes.toolbar}>
           <IconButton
             size="large"
             edge="start"
@@ -51,43 +64,47 @@ export default function MenuAppBar() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <img src={cwLogo} alt="logo" className={classes.logo} />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Photos
+          <Typography
+            className={classes.title}
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+          >
+            ──── <span>{"<BlueBerry/>"}</span> BLOG ────
           </Typography>
-          {auth && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          )}
+
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle fontSize="large" />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+            </Menu>
+          </div>
         </Toolbar>
       </AppBar>
     </Box>
---
+++ fireblog-app/src/utils/functions.js
@@ -0,0 +1,7 @@
+import firebase from "./firebase";
+
+import { getDatabase } from "firebase/database";
+
+export const addUser = () => {
+  const db = getDatabase();
+};
