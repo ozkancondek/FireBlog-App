3767cf6d370cd0b10c24657cdc2e86b947bb3e09
ozkancondek <ozkncndek@gmail.com>
2022-04-09 16:01:46 +0200

    firebase credentials and card height

--- fireblogapp2/src/components/BlogCard.js
+++ fireblogapp2/src/components/BlogCard.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles(() => ({
       "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset",
   },
   media: {
-    height: 140,
+    height: 200,
   },
   module: {
     display: "-webkit-box",
